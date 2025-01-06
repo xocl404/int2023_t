53bb36e6650f92af49ebb42dd9994d5b7a69a65f
Yaroslav Sidyakin <144961952+xocl404@users.noreply.github.com>
2025-01-07 00:13:58 +0500

    Update number.h

--- lib/number.h
+++ lib/number.h
@@ -5,11 +5,13 @@
 
 
 struct int2023_t {
-    // implement
-
+    bool sign = true;
+    uint8_t arr[252]{};
 };
 
 static_assert(sizeof(int2023_t) <= 253, "Size of int2023_t must be no higher than 253 bytes");
+
+int GetSize (const int2023_t &number);
 
 int2023_t from_int(int32_t i);
 
@@ -26,5 +28,7 @@ int2023_t operator/(const int2023_t& lhs, const int2023_t& rhs);
 bool operator==(const int2023_t& lhs, const int2023_t& rhs);
 
 bool operator!=(const int2023_t& lhs, const int2023_t& rhs);
+
+bool operator>(const int2023_t& lhs, const int2023_t& rhs);
 
 std::ostream& operator<<(std::ostream& stream, const int2023_t& value);
