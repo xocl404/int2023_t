cfd2f31a2e0cb6e5d3676dbbed1141f6a60197e0
Yaroslav Sidyakin <144961952+xocl404@users.noreply.github.com>
2025-01-06 17:18:39 +0500

    Add files via upload

---
+++ lib/number.cpp
@@ -0,0 +1,47 @@
+#include "number.h"
+
+int2023_t from_int(int32_t i) {
+    
+    return int2023_t();
+}
+
+int2023_t from_string(const char* buff) {
+    
+    return int2023_t();
+}
+
+int2023_t operator+(const int2023_t& lhs, const int2023_t& rhs) {
+    
+    return int2023_t();
+}
+
+int2023_t operator-(const int2023_t& lhs, const int2023_t& rhs) {
+  
+    return int2023_t();
+}
+
+int2023_t operator*(const int2023_t& lhs, const int2023_t& rhs) {
+    
+    return int2023_t();
+}
+
+int2023_t operator/(const int2023_t& lhs, const int2023_t& rhs) {
+
+    return int2023_t();
+}
+
+bool operator==(const int2023_t& lhs, const int2023_t& rhs) {
+    
+    return true;
+}
+
+bool operator!=(const int2023_t& lhs, const int2023_t& rhs) {
+    
+    return false;
+}
+
+std::ostream& operator<<(std::ostream& stream, const int2023_t& value) {
+    stream << value;
+
+    return stream;
+}
